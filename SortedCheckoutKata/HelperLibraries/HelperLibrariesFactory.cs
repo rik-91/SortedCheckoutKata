11e1b9304862b9e1f4feedacbdf633e7b58f824d
Rik <richard.aj.hodges>
2019-10-31 20:19:25 +0000

    1. Implemented OrderLinePriceCalculatorWithoutDiscount.Calculate method.
    2. Updated HelperLibrariesFactory.GetOrderLinePriceCalculator method to provide a IKataItemsDatabaseAccess implementation to the IOrderLinePriceCalculator objects.

--- SortedCheckoutKata/HelperLibraries/HelperLibrariesFactory.cs
+++ SortedCheckoutKata/HelperLibraries/HelperLibrariesFactory.cs
@@ -1,4 +1,6 @@
-﻿using SortedCheckoutKata.Enums;
+﻿using SortedCheckoutKata.Database;
+using SortedCheckoutKata.Database.KataItemsDatabaseAccess;
+using SortedCheckoutKata.Enums;
 using SortedCheckoutKata.HelperLibraries.OrderLinePriceCalculator;
 using System;
 
@@ -8,12 +10,14 @@ namespace SortedCheckoutKata.HelperLibraries
     {
         internal static IOrderLinePriceCalculator GetOrderLinePriceCalculator(PriceCalculationType priceCalculationType)
         {
+            IKataItemsDatabaseAccess kataItemsDatabaseAccess = DatabaseFactory.GetKataItemsDatabaseAccess(KataItemsDatabaseAccessType.MockData);
+
             switch (priceCalculationType)
             {
                 case PriceCalculationType.WithoutDiscount:
-                    return new OrderLinePriceCalculatorWithoutDiscount();
+                    return new OrderLinePriceCalculatorWithoutDiscount(kataItemsDatabaseAccess);
                 case PriceCalculationType.WithDiscount:
-                    return new OrderLinePriceCalculatorWithDiscount();
+                    return new OrderLinePriceCalculatorWithDiscount(kataItemsDatabaseAccess);
                 default:
                     throw new Exception("Invalid PriceCalculationType enum provided to SortedCheckoutKata.HelperLibraries.HelperLibrariesFactory.GetOrderLinePriceCalculator method.");
             }
--- SortedCheckoutKata/HelperLibraries/OrderLinePriceCalculator/OrderLinePriceCalculatorWithoutDiscount.cs
+++ SortedCheckoutKata/HelperLibraries/OrderLinePriceCalculator/OrderLinePriceCalculatorWithoutDiscount.cs
@@ -1,12 +1,20 @@
 ﻿using SortedCheckoutKata.BusinessObjects;
+using SortedCheckoutKata.Database.KataItemsDatabaseAccess;
 
 namespace SortedCheckoutKata.HelperLibraries.OrderLinePriceCalculator
 {
     internal class OrderLinePriceCalculatorWithoutDiscount : IOrderLinePriceCalculator
     {
+        private IKataItemsDatabaseAccess _kataItemsDatabaseAccess = null;
+
+        public OrderLinePriceCalculatorWithoutDiscount(IKataItemsDatabaseAccess kataItemsDatabaseAccess)
+        {
+            _kataItemsDatabaseAccess = kataItemsDatabaseAccess;
+        }
+
         public decimal Calculate(Item item, int itemQty)
         {
-            return 0;
+            return _kataItemsDatabaseAccess.CalculateTotalPrice(item.Sku, itemQty);
         }
     }
 }
