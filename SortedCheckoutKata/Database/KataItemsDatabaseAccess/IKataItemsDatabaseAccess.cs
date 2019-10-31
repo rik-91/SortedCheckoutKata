569cec290e47273aadb0efeaae0c1ac62419cc10
Rik <richard.aj.hodges>
2019-10-31 07:32:46 +0000

    1. Setup initial database classes.

---
+++ SortedCheckoutKata/Database/KataItemsDatabaseAccess/IKataItemsDatabaseAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace SortedCheckoutKata.Database.KataItemsDatabaseAccess
+{
+    internal interface IKataItemsDatabaseAccess
+    {
+        decimal CalculateTotalPrice(string sku, int itemQty);
+        decimal CalculateDiscountPrice(string sku, int itemQty);
+    }
+}
---
+++ SortedCheckoutKata/Database/KataItemsDatabaseAccess/KataItemsDatabaseAccess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedCheckoutKata.Database.KataItemsDatabaseAccess
+{
+    internal class KataItemsDatabaseAccess : IKataItemsDatabaseAccess
+    {
+        public KataItemsDatabaseAccess()
+        {
+        }
+
+        public decimal CalculateTotalPrice(string sku, int itemQty)
+        {
+            return 0;
+        }
+
+        public decimal CalculateDiscountPrice(string sku, int itemQty)
+        {
+            return 0;
+        }
+    }
+}
